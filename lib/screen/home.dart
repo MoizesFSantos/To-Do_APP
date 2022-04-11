74ce07788e0291c8bf0528488ab22a9d4478e4c9
Moizes Ferreira dos Santos <moizesferreira66@hotmail.com>
2022-04-11 17:32:56 -0300

    first commit

---
+++ lib/screen/home.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(230, 233, 233, 233),
+      appBar: AppBar(
+        title: const Text(
+          'HOME',
+          style: TextStyle(color: Color.fromARGB(246, 81, 199, 128)),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          children: [],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
