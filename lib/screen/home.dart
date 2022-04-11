e301dd7f231f16f41c15040f98054e9b4a20be6b
Moizes Ferreira dos Santos <moizesferreira66@hotmail.com>
2022-04-11 18:18:38 -0300

    feat: create drawer navigation

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -12,7 +12,23 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(230, 233, 233, 233),
+      drawer: Drawer(),
       appBar: AppBar(
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(
+                Icons.menu,
+                color: Color.fromARGB(246, 81, 199, 128),
+                size: 30,
+              ),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          },
+        ),
+        centerTitle: true,
         title: const Text(
           'HOME',
           style: TextStyle(color: Color.fromARGB(246, 81, 199, 128)),
@@ -20,14 +36,13 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.transparent,
         elevation: 0.0,
       ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          children: [],
+      body: const Center(
+        child: SingleChildScrollView(
+          child: Text('Task'),
         ),
       ),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: const Color.fromARGB(246, 81, 199, 128),
         onPressed: () => {},
         tooltip: 'Increment',
         child: const Icon(Icons.add),
